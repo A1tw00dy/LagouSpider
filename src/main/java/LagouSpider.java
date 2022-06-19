fec309edaa6a7fd2956789d357137f1de9bc8746
A1tw00dy <403215130@qq.com>
2022-06-19 19:10:22 +0800

    2022/06/19

--- src/main/java/LagouSpider.java
+++ src/main/java/LagouSpider.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LagouSpider {
     public static void main(String[] args) {
+        System.out.println("hello");
         //设置webdriver路径
         System.setProperty("webdriver.chrome.driver",LagouSpider.class.getClassLoader().getResource("chromedriver.exe").getPath());
         //创建webdriver
